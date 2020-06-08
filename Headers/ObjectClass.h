21d754282aaae3928fc771ebe9be855c58f1487f
KrishnaMohan454 <ankeshwarkrishna@gmail.com>
2020-06-08 17:54:20 +0530

    Add files via upload
    added files manually

---
+++ Headers/ObjectClass.h
@@ -0,0 +1,18 @@
+//
+//  ObjectClass.h
+//  TestFramework
+//
+//  Created by AID011 on 08/06/20.
+//  Copyright © 2020 AID011. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface ObjectClass : NSObject
+
++(void)showFrameworkNameInApp;
+
+@end
+
+
